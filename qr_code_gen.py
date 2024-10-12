dd32de0f77c6d5029d06147822049d49838a885c
Edward Watson <117376879+solitary8@users.noreply.github.com>
2024-10-12 10:53:05 +0000

    changed the code

--- qr_code_gen.py
+++ qr_code_gen.py
@@ -4,4 +4,10 @@ data = 'hello'
 
 img = qrcode.make(data)
 
+qr = qrcode.QRCode(version = 1, box_size = 10, border = 5)
+qr.add_data(data)
+qr.make(fit = True)
+img = qr.make_image(fill_color = 'blue', back_color = 'white')
+
+
 img.save('/home/codespace/test.png')
